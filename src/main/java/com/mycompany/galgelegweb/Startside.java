14cd6f79970f3c20ebf0b755651679479615c287
viktor <s164930@student.dtu.dk>
2018-03-13 11:36:25 +0100

    Første commit

---
+++ src/main/java/com/mycompany/galgelegweb/Startside.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.galgelegweb;
+
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Date;
+import java.util.HashMap;
+/**
+ *
+ * @author viktor
+ */
+@Path("")
+public class Startside {
+    
+    @GET
+    public String getIndexHtml() throws IOException {
+        //Initialize Mustache renderer
+        MustacheFactory mf = new DefaultMustacheFactory();
+        Mustache m = mf.compile("startside.mustache");
+
+        Todo todo = new Todo("Simon er dum", "Description");
+        
+        HashMap<String, Object> data = new HashMap<String, Object>();
+        data.put("todo", todo);
+        
+        StringWriter writer = new StringWriter();
+        m.execute(writer, data).flush();
+        String html = writer.toString();
+        return html;
+    }
+}
